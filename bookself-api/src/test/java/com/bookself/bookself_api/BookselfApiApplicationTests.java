8424a9a866157e57ef81bbd0061288f0470b8018
Niladri Sekhar Jyoti <niladri123@gmail.com>
2024-09-04 19:27:36 -0400

    Starter Code Added

---
+++ bookself-api/src/test/java/com/bookself/bookself_api/BookselfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookself.bookself_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookselfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
