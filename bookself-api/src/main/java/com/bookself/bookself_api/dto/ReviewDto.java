05adecb2b4e6b0db105aed8abbaf05a88cc81476
wisevaishu <vpm.win@gmail.com>
2024-11-12 19:54:39 -0600

    Completed Review Dto and Mappers

---
+++ bookself-api/src/main/java/com/bookself/bookself_api/dto/ReviewDto.java
@@ -0,0 +1,71 @@
+package com.bookself.bookself_api.dto;
+
+import java.time.LocalDateTime;
+
+public class ReviewDto {
+    private Long id;
+    private Long userId;
+    private Long bookId;
+    private String content;
+    private int rating;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
